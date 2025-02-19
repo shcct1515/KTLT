a66179e2f7ce0b0e1e0bc91fd851dcdf390add7d
shcct <shcct1254@gmail.com>
2025-02-19 22:24:39 +0700

    update

--- c.py
+++ c.py
@@ -1,8 +1,16 @@
 import os
-def createfile(direction, content):
+""" def createfile(direction, content):
     if os.path.exists(direction):
         f = open(direction, "a", encoding="utf8")
     else:
+        f= open(direction, "w", encoding="utf8")
+    f.write(content)
+    f.close() """
+
+def createfile(direction, content):
+    try:
+        f = open(direction, "a", encoding="utf8")
+    except FileNotFoundError:
         f= open(direction, "w", encoding="utf8")
     f.write(content)
     f.close()
